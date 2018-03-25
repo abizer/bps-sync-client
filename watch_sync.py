c0fe15bb5b54438dbe159a20a97883c63787f11c
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-03-25 16:50:46 -0700

    add config file

--- watch_sync.py
+++ watch_sync.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+import configparser
 import inotify.adapters
 
-WATCH_DIR="test"
+ENV="dev" # or "prod"
+CONF_FILE = 'sync.conf'
 
-def main():
+
+def main(watch_dir, tmux_socket, tmux_session, remote_dir, transfer_queue, transfer_log):
+    print watch_dir, tmux_socket, tmux_session, remote_dir, transfer_queue, transfer_log
+    
     _i = inotify.adapters.Inotify()
 
-    _i.add_watch(WATCH_DIR)
+    _i.add_watch(watch_dir)
     
-    print "Established watches on {}...".format(WATCH_DIR)
+    print "Established watches on {}...".format(watch_dir)
 
     for event in _i.event_gen():
         if event is not None:
@@ -19,4 +24,17 @@ def main():
                 print "path={}, filename={}, type_names={}".format(path, filename, type_names)
 
 if __name__ == '__main__':
-    exit(main())
+
+    conf = configparser.ConfigParser()
+    conf.read(CONF_FILE)
+    
+    exit(
+        main(
+            watch_dir = conf.get(ENV, 'watch_dir'),
+            tmux_socket = conf.get(ENV, 'tmux_socket'),
+            tmux_session = conf.get(ENV, 'tmux_session'),
+            remote_dir = conf.get(ENV, 'remote_dir'),
+            transfer_queue = conf.get(ENV, 'transfer_queue'),
+            transfer_log = conf.get(ENV, 'transfer_log')
+        )
+    )
