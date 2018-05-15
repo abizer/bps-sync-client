2fba28dd57c512f18447b490a7624f84b94c03a0
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-05-15 00:31:18 -0700

    various updates

--- watch_sync.py
+++ watch_sync.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!./venv/bin/python
 
 from datetime import datetime
 import configparser
@@ -8,45 +8,13 @@ import libtmux
 from textwrap import dedent
 import time
 
-ENV="dev" # or "prod"
+ENV = os.environ.get('BPS_SYNC_ENV', 'dev')
 CONF_FILE = 'sync.conf'
 
-def main(watch_dir, tmux_socket, tmux_session, remote_dir, transfer_queue, transfer_log, transfer_script):
-
-    # generate the script we use in tmux to handle the transfer itself
-    _build_transfer_script(transfer_script, transfer_queue, transfer_log, remote_dir)
-
-    _i = inotify.adapters.Inotify()
-
-    _i.add_watch(watch_dir)
-
-    print "Established watches on {}...".format(watch_dir)
-
-    tmux = libtmux.Server(socket_path=tmux_socket)
-    transfer_session = tmux.find_where({ "session_name": tmux_session })
-    transfer_script = 'transfer.sh' if ENV is "prod" else "dev_transfer.sh"
-
-    for event in _i.event_gen():
-        if event is not None:
-            (_, type_names, path, filename) = event
-
-            fqfn = os.path.join(path, filename)
-
-            # only interested in moves into the folder
-            if "IN_MOVED_TO" in type_names:
-                print "{}: received {} for transfer".format(datetime.now(), fqfn)
-                try:
-                    # execute the transfer script in a tmux session
-                    window = transfer_session.new_window(window_shell='bash {} {}'.format(transfer_script, fqfn))
-                    while True:
-                        print window.id, window.active
-                        time.sleep(2)
-
-                except Exception as e:
-                    print(e)
 
 def _schedule_transfer():
     pass
+
 
 def _build_transfer_script(filename, transfer_queue, transfer_log, remote_dir):
     template = dedent("""
@@ -66,19 +34,70 @@ def _build_transfer_script(filename, transfer_queue, transfer_log, remote_dir):
     with open(filename, 'w') as transfer_file:
         transfer_file.write(template)
 
+
+def main(watch_dir, tmux_socket, tmux_session, remote_dir, transfer_queue, transfer_log, transfer_script):
+
+    # generate the script we use in tmux to handle the transfer itself
+    _build_transfer_script(
+        transfer_script, transfer_queue, transfer_log, remote_dir)
+
+    _i = inotify.adapters.Inotify()
+
+    _i.add_watch(watch_dir)
+
+    print("Run Environment: {}".format(ENV))
+    print("tmux Socket: {}, Session: {}".format(tmux_socket, tmux_session))
+    print("Established watches on {}...".format(watch_dir))
+
+    tmux = libtmux.Server(socket_path=tmux_socket)
+
+    if not tmux:
+        print("Could not connect to tmux server on socket {}".format(tmux_socket))
+        return 1
+
+    transfer_session = tmux.find_where({"session_name": tmux_session})
+
+    if not transfer_session:
+        print("Could not find tmux session {} on socket {}".format(
+            tmux_session, tmux_socket))
+        return 1
+
+    transfer_script = 'transfer.sh' if ENV is "prod" else "dev_transfer.sh"
+
+    for event in _i.event_gen():
+        if event is not None:
+            (_, type_names, path, filename) = event
+
+            fqfn = os.path.join(path, filename)
+
+            # only interested in moves into the folder
+            if "IN_MOVED_TO" in type_names:
+                print("{}: received {} for transfer to {}".format(
+                    datetime.now(), fqfn, remote_dir))
+                try:
+                    # execute the transfer script in a tmux session
+                    window = transfer_session.new_window(
+                        window_shell='bash {} {}'.format(transfer_script, fqfn))
+
+                except Exception as e:
+                    print(e)
+
+
 if __name__ == '__main__':
 
     conf = configparser.ConfigParser()
     conf.read(CONF_FILE)
-    
+
+    def _conf(key): return conf.get(ENV, key)
+
     exit(
         main(
-            watch_dir = conf.get(ENV, 'watch_dir'),
-            tmux_socket = conf.get(ENV, 'tmux_socket'),
-            tmux_session = conf.get(ENV, 'tmux_session'),
-            remote_dir = conf.get(ENV, 'remote_dir'),
-            transfer_queue = conf.get(ENV, 'transfer_queue'),
-            transfer_log = conf.get(ENV, 'transfer_log'),
-            transfer_script = conf.get(ENV, 'transfer_script'),
+            watch_dir=_conf('watch_dir'),
+            tmux_socket=_conf('tmux_socket'),
+            tmux_session=_conf('tmux_session'),
+            remote_dir=_conf('remote_dir'),
+            transfer_queue=_conf('transfer_queue'),
+            transfer_log=_conf('transfer_log'),
+            transfer_script=_conf('transfer_script'),
         )
     )
